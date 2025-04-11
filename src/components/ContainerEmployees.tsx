1a80626fcdc5f748479839554730e80ca663c47e
Pipe Jara <pipejarauribeboteroa@gmail.com>
2025-04-11 18:41:59 -0500

    ready

--- src/components/ContainerEmployees.tsx
+++ src/components/ContainerEmployees.tsx
@@ -1,20 +1,32 @@
-import { Data } from "../interfaces";
+import { Data, Date } from "../interfaces";
 import { CardItem } from "./CardItem";
 
 interface Props {
   items: Data[];
-  hanleDragging: (dragging: boolean)=> void
+  hanleDragging: (dragging: boolean) => void;
+  hanleUpdateList: (id: number, date: Date) => void;
 }
 
-export const ContainerEmployees = ({ items = [], hanleDragging }: Props) => {
+export const ContainerEmployees = ({
+  items = [],
+  hanleDragging,
+  hanleUpdateList,
+}: Props) => {
   return (
     <div className="layout-cards containerEmployees">
       <p>Employees</p>
       <div className="all">
-      {items.map(
-        (item) => "" === item.date && 
-        <CardItem data={item} key={item.id} handleDragging={hanleDragging}/>
-      )}
+        {items.map(
+          (item) =>
+            "" === item.date && (
+              <CardItem
+                data={item}
+                key={item.id}
+                handleDragging={hanleDragging}
+                handleUpdateList={hanleUpdateList}
+              />
+            )
+        )}
       </div>
     </div>
   );
