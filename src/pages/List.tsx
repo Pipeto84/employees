f3959cee4f78d7fbb110d07f1aaacae618b58ad9
Pipe Jara <pipejarauribeboteroa@gmail.com>
2025-04-25 17:05:30 -0500

    don't edit employee

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -2,12 +2,8 @@ import "../styles/List.css";
 import { CardLIst } from "../components/CardLIst";
 import { employeesList } from "../assets/index";
 import { EditEmployee } from "../components/EditEmployee";
-import { useEffect } from "react";
 
 export const List = () => {
-  useEffect(() => {
-    console.log(employeesList)
-  }, [employeesList])
   
   return (
     <div className="list">
